b63ea08acf9da203472ebf3edc3358ca46c96083
Sesamestrong <doolie.healey@gmail.com>
2019-02-27 18:09:45 -0500

    Set this repo to the recent version on Repl.it.

--- src/App.js
+++ src/App.js
@@ -2,86 +2,182 @@ import React, {
     Component
 } from 'react';
 import './App.css';
-
+//Main root of app
 class App extends Component {
     render() {
-        return (<div className = "App">
-            <h1>Todo</h1><TodoList/></div>);
+        return (<div className="App">
+      <TodoList /></div>);
     }
 }
+//Any todo list - takes in no parameters
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             todo: {},
         };
-        this.addTodo('R&E Packet', '30 min-1 hr', {
-            color: 'orange',
-            text: 'High',
-        });
-        this.addTodo('Debate Homework', '30 min-1 hr', {
-            color: 'red',
-            text: 'Very high',
-        });
+    }
+    componentDidMount(){
+      this.setState({todo:JSON.parse(document.cookie||"{}")});
+      window.onbeforeunload=()=>{
+        document.cookie=JSON.stringify(this.state.todo);
+      }
     }
     render() {
+        let todos = Object.keys(this.state.todo).map(i => (
+            <Todo timeFrame={this.state.todo[i].time}
+              key={i}
+              urgency={this.state.todo[i].urgency}
+	      color={this.state.todo[i].color}
+              checked={this.state.todo[i].checked}
+	      update={(name,value)=>this.update(i,name,value)}
+	      check={() => this.check(i)} text={this.state.todo[i].text}
+     	      editing={this.state.todo[i].editing} 
+	      delete={()=>this.delete(i)}
+            />));
         return (
-            <div className = "todo-list" > {
-		    Object.keys(this.state.todo).map(i => (this.state[i].done ? '' :
-			    <Todo timeFrame = {this.state[i].time}
-			    urgency = {this.state[i].urgency}
-			    check={() =>this.check(i)}>
-			    	{this.state[i].text}
-			    </Todo>))
-			}
-		    </div>);
+            <div className="TodoList">
+        <h1>Todo</h1>
+	{todos.length===0?"Press the Plus button below to add a Todo.":todos
+        }
+	<h1 style={{fontSize:"50px"}} onClick={()=>this.addTodo()}>+</h1>
+      </div>);
+    }
+    componentDidUpdate(){
+      
+    }
+    testValues() { //Add R&E and debate homework
+        let todos = [this.Todo('R&E Packet', '30 min-1 hr',
+            'High',
+            'orange',
+        ), this.Todo('Debate Homework', '30 min',
+            'Very high',
+            'red',
+        )];
+        this.setState({
+            todo: Object.assign({}, this.state.todo, ...todos)
+        });
+    }
+    addTodo() {
+        this.setState({
+            todo: Object.assign({}, this.state.todo, this.Todo("", "", "", "white"))
+        });
     }
     check(id) {
         this.setState({
-            [id]: {
-                ...this.state[id],
-                checked: !this.state[id].checked,
+            todo: Object.assign({}, this.state.todo, {
+                [id]: {
+                    ...this.state.todo[id],
+                    checked: !this.state.todo[id].checked,
+                },
+            })
+        });
+    }
+    delete(id) {
+        let {
+            [id]: del, ...rest
+        } = this.state.todo;
+        console.log(rest);
+        this.setState({
+            todo: rest
+        });
+    }
+    Todo(text, time, urgency, color) {
+        return ({
+            [Math.random()]: {
+                time,
+                urgency,
+                color,
+                text,
+                checked: false,
+                editing:true
             },
         });
     }
-    addTodo(text, time, urgency) {
+    update(id, name, value) {
         this.setState({
-            todo: Object.assign({
-                [Math.random()]: {
-                    time,
-                    urgency,
-                    text,
-                    checked: false,
-                },
-            }, this.state.todo),
-        });
+            todo: Object.assign({}, this.state.todo, {
+                [id]: Object.assign({}, this.state.todo[id], {
+                    [name]: value
+                })
+            })
+        })
     }
 }
-
+//Any todo element - takes in a color, a callback function for changes, urgency, name and time
 class Todo extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            editing: this.props.editing === true
+        };
+    }
+    toggleEdit() {
+        let editing=this.state.editing;
+        this.setState({
+            editing: !editing
+        });
+        this.props.update("editing",!editing);
+    }
+    componentDidMount(){
+      this.refs.checkBox.checked=this.props.checked;
     }
     render() {
-        return (<div className = "todo"
-                                    style = {
-                                        'background-color:{this.props.urgency.color}'
-                                    } >
-                                    <span className = "todo-component check-container" >
-                                    <input className = "check"
-                                    type = "checkbox"
-                                    onChange = {
-                                        this.props.check
-                                    }/></span><span className = "todo-component desc-container" > {
-                                        this.props.innerText
-                                    } </span> <span className="todo-component urgency-container">
-                                    Urgency: {
-                                        this.props.urgency.text
-                                    } </span> <span className="todo-component timeFrame-container">
-                                    Time Estimate: {
-                                        this.props.timeFrame
-                                    } </span> </div>);
+        return <div className="Todo" style={{backgroundColor:this.props.checked?"lightgreen":this.props.color}}>
+      <span className="checkContainer">
+        <input className="check MutableValue"
+          type="checkbox"
+          onChange={
+            this.props.check
+	  } ref="checkBox" />
+	</span>
+		    {this.props.checked ? ("Done"): (
+		    <>
+			    <span className="colorContainer">
+				    <MutableValue type="color" value={this.props.color} hidden={true} callback={val=>this.props.update("color",val)} editing={this.state.editing}/>
+			    </span>
+	<span className="descContainer">
+		<MutableValue value={this.props.text} name="Description" callback={val=>this.props.update("text",val)} editing={this.state.editing} />
+				</span>
+          <span className="urgencyContainer">
+		  Urgency: <MutableValue value={
+          this.props.urgency
+		  } callback={val=>this.props.update("urgency",val)} editing={this.state.editing} />
+	  </span>
+        <span className="timeFrameContainer">
+		Time Estimate: <MutableValue value={
+          this.props.timeFrame
+		} callback={val=>this.props.update("time",val)} editing={this.state.editing} />
+	</span>
+        <span className="MutableValue">
+	<input type="button" value="✎" onClick={()=>{this.toggleEdit();}}/>
+		    </span>
+			    <span>
+				    <button onClick={this.props.delete}><span role="img" aria-label="X">❌</span></button>
+			    </span>
+    </>)}
+    </div>;
+    }
+}
+//Any mutable value - takes in a starting value and a callback function (for changing the value and storing it somewhere)
+class MutableValue extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: this.props.value
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+    render() {
+        return <span className="MutableValue">{this.props.editing ?
+		    (<input type={this.props.type||"text"} placeholder={this.props.name||""} value={this.props.value} size={this.state.value.length} onChange={this.onChange}/>) :
+			    (this.props.hidden===true?undefined:this.state.value)}</span>;
+    }
+    onChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+        this.props.callback(event.target.value);
     }
 }
 export default App;
